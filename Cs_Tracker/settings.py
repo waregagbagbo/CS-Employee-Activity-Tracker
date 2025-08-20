900c5bd206d18b4671a3073958e4f585e563ffff
waregagbagbo <gabelwarega@gmail.com>
2025-08-20 17:30:58 +0300

    Updated the models on the account app

--- Cs_Tracker/settings.py
+++ Cs_Tracker/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'accounts',
+    'Employee_Tracker',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +123,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,48 @@
+from django.conf import settings
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+USER_TYPE =[
+    ('Supervisor', 'Supervisor'),
+    ('Employee_Agent', 'Employee_Agent'),
+    ('Admin', 'Admin'),
+]
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    hire_date = models.DateField(null=True, blank=True)
+    bio = models.TextField()
+    avatar = models.ImageField(upload_to='avatars/')
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'CustomUser'
+        verbose_name = 'CustomUser'
+        verbose_name_plural = 'CustomUsers'
+        ordering = ['email']
+        constraints = [
+            models.UniqueConstraint(
+                fields=['email'],
+                name='unique_email'
+            )
+        ]
+
+class Department(models.Model):
+    title = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
+
+
+class Employee(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    employee_id = models.IntegerField(unique=True)
+    department = models.ForeignKey('Department', on_delete=models.CASCADE)
+    shift_start_time = models.DateTimeField(auto_now_add=True)
+    shift_end_time = models.DateTimeField(auto_now_add=True)
+    user_type = models.CharField(max_length=20, choices=USER_TYPE, default='Employee_Agent')
+    supervisor = models.ForeignKey(CustomUser, on_delete=models.CASCADE,related_name='supervised_employees')
+
+    def __str__(self):
+        return f"{self.user.username} - {self.employee_id}"
+
