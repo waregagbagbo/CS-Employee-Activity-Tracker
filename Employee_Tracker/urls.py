aa18837ce7aa98bdabb345360c1a7d8121cd7d7b
waregagbagbo <gabelwarega@gmail.com>
2025-08-22 12:24:05 +0300

    shiftef to JWT authentication class and updated the urs

--- Employee_Tracker/urls.py
+++ Employee_Tracker/urls.py
@@ -1,8 +1,11 @@
 from tkinter.font import names
 
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+TokenObtainPairView,TokenRefreshView,)
 from django.urls import path,include
 from Employee_Tracker import views
+
 
 router = DefaultRouter()
 router.register(r'department', views.DepartmentAPIViewSet, basename='department')
@@ -17,4 +20,10 @@ urlpatterns =[
     path('profile_update',views.EmployeeProfileAPIUpdate.as_view(), name='profile_update'),
     path('shifts',views.ShiftAPIView.as_view(), name='shift'),
     path('shift/update',views.ShiftAPIUpdate.as_view(), name='shift_update'),
+
+    # for tokens
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+
 ]
