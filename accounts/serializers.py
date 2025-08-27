0a3b19f6f2a9e8156a432b38a4842745ffcbc7c1
waregagbagbo <gabelwarega@gmail.com>
2025-08-27 17:51:48 +0300

    fixed the attribute error on UserRegistration serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,11 +2,11 @@ from rest_framework import serializers
 from Cs_Tracker.settings import AUTH_USER_MODEL
 from django.apps import apps
 
-user = apps.get_model(AUTH_USER_MODEL) # create an object from the AUTH_USER_MODEK
+User = apps.get_model(AUTH_USER_MODEL) # create an object from the AUTH_USER_MODEK
 # create user serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = AUTH_USER_MODEL
+        model = User
         fields = ('username', 'email', 'first_name', 'last_name')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
@@ -14,7 +14,7 @@ class UserSerializer(serializers.ModelSerializer):
         # To check the status of the object
         if self.instance is None:
             # To create the
-            if user.objects.filter(email=value).exists():
+            if User.objects.filter(email=value).exists():
                 raise serializers.ValidationError('Email already registered')
             else:
                 return value
@@ -24,8 +24,8 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     password_confirmation = serializers.CharField(write_only=True)
     class Meta:
-        model = AUTH_USER_MODEL
-        fields = ('username', 'email', 'first_name', 'last_name')
+        model = User
+        fields = ('username', 'email', 'first_name', 'last_name','password', 'password_confirmation')
         extra_kwargs = {'password': {'write_only': True, 'min_length': 5}}
 
     def validate_password(self, value):
