f0261007301ecd804caff30a8c884062baea2025
waregagbagbo <gabelwarega@gmail.com>
2025-08-27 17:40:18 +0300

    added the session authentication alongside jwt

--- Cs_Tracker/settings.py
+++ Cs_Tracker/settings.py
@@ -135,6 +135,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         #   'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     # this one handles pagination
