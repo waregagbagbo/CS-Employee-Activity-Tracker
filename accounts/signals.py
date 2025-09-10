a9fbfb6b7b4b95778a47be19d7cd1e9b7ee0d6cc
waregagbagbo <gabelwarega@gmail.com>
2025-09-10 21:48:55 +0300

    Refactored to use the get_user_model instead of Auth user

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,17 +1,20 @@
+from django.contrib.auth import get_user_model
 from django.dispatch import receiver
 from django.conf import settings
 from django.db.models.signals import post_save
 from rest_framework.authtoken.models import Token
 from accounts.models import Employee, Department
 
+User = get_user_model()
+
 # handle auto token generation
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=User)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
        Token.objects.create(user=instance)
 
 # handle auto profile creation
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+@receiver(post_save, sender=User)
 def create_user_profile(sender, instance=None, created=False, **kwargs):
     if created and not hasattr(instance, 'employee_profile'): # ensures handling duplicates
         assign_dept, _= Department.objects.get_or_create(name='Tech')
