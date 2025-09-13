3d5ecd6e5f95f93ded02ea607ff26d5d5b25c073
waregagbagbo <gabelwarega@gmail.com>
2025-09-13 14:45:36 +0300

    modified the pagination class

--- Cs_Tracker/settings.py
+++ Cs_Tracker/settings.py
@@ -160,7 +160,7 @@ REST_FRAMEWORK = {
     # this one handles pagination
     'DEFAULT_PAGINATION_CLASS':
         'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 10,
+        'PAGE_SIZE': 5,
 
     # for default permission class
     'DEFAULT_PERMISSION_CLASSES': [
