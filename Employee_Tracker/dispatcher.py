1fc4923f5115afe3b30c75fcfb6cef51b5ff9fd9
waregagbagbo <gabelwarega@gmail.com>
2025-09-20 23:56:35 +0300

    added the registry module that has the events and formatter function to return model instance into the intended payload

--- Employee_Tracker/dispatcher.py
+++ Employee_Tracker/dispatcher.py
@@ -1,19 +1,17 @@
 import requests
 
-# create a method to handle the webhook
+
 """event is what we want, payload is the future of what we might add, destination is the target"""
 
-def webhook_dispatcher(event,payload, destination):
+def webhook_dispatcher(event,payload, destination):  # create a method to handle the webhook
     if destination == 'slack':
-        # do inline import to prevent circular module
-        from slack import slack_trigger
+        from slack import slack_trigger # do inline import to prevent circular module
         slack_trigger(payload)
-
     else:
         try:
             requests.post(destination,json=payload)
         except requests.exceptions.RequestException as e:
             print('Webhook dispatch failed {e}')
 
-        
+
 
---
+++ Employee_Tracker/registry.py
@@ -0,0 +1,10 @@
+# this module maps events to destinations and formatters
+""" Shift is model instance used so that it get the payload body"""
+EVENTS = {
+    'shift_status_changed': {
+        'destination': 'slack',
+        'formatter': lambda shift: {
+            "text": f"🔔 Shift status changed for *{shift.user.username}*: `{shift.old_status}` → `{shift.status}`"
+        }
+    }
+}
