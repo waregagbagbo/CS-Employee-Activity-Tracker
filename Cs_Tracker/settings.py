a813d3eb9355000e4ef20a54f76c57020be28c17
waregagbagbo <gabelwarega@gmail.com>
2025-10-31 15:57:52 +0300

    Fixed the .env file configuration

--- Cs_Tracker/settings.py
+++ Cs_Tracker/settings.py
@@ -26,14 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY=config('SECRET_KEY')
 
-#SECRET_KEY = 'django-insecure-8@sot$57g^*=m-^pin0gqnl6okr2htr=v15sp=_$uc-o-)29as'
-
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-#DEBUG = True
+DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost', cast=Csv())
-#ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1', cast=Csv())
 
 SITE_ID = 1
 
