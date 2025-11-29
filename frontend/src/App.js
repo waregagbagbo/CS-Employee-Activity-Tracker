d7cdf04b6274bbce0d995994e65f49230398711e
waregagbagbo <gabelwarega@gmail.com>
2025-11-30 01:13:25 +0300

    modified the App folder;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Employees from "./pages/Employees";
-import Shifts from "./pages/Shifts";
-import Reports from "./pages/Reports";
+import Activities from "./pages/Activities";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-function App(){
+export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+
         <Route path="/login" element={<Login />} />
-        <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        <Route path="/employees" element={<ProtectedRoute><Employees /></ProtectedRoute>} />
-        <Route path="/shifts" element={<ProtectedRoute><Shifts /></ProtectedRoute>} />
-        <Route path="/reports" element={<ProtectedRoute><Reports /></ProtectedRoute>} />
+
+        <Route path="/dashboard" element={
+          <ProtectedRoute><Dashboard /></ProtectedRoute>
+        } />
+
+        <Route path="/employees" element={
+          <ProtectedRoute><Employees /></ProtectedRoute>
+        } />
+
+        <Route path="/activities" element={
+          <ProtectedRoute><Activities /></ProtectedRoute>
+        } />
+
       </Routes>
     </BrowserRouter>
   );
 }
-
-export default App;
