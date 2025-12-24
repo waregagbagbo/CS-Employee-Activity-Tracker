9821630c9122e3dd0a40b2971cfcee712c61b3eb
waregagbagbo <gabelwarega@gmail.com>
2025-12-25 02:42:17 +0300

    modified the setings and env file

--- Cs_Tracker/urls.py
+++ Cs_Tracker/urls.py
@@ -19,7 +19,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cs/',include('Employee_Tracker.urls')),
+    path('api/',include('Employee_Tracker.urls')),
     path('',include('accounts.urls')),
 
 
