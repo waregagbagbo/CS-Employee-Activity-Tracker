286441cb7f1fb8bcd63efac2b103fcab2bcb3e7d
waregagbagbo <gabelwarega@gmail.com>
2026-01-21 12:23:20 +0300

    added the me endpoint for profile

--- frontend/src/services/employee.js
+++ frontend/src/services/employee.js
@@ -1,5 +1,6 @@
 import API from "../services/api"
 
 export const listEmployees = (params) => API.get("api/employees/", { params });
+export const getMyProfile = () => API.get("api/employee/me/");
 export const retrieveEmployee = (id) => API.get(`api/employees/${id}/`);
 export const updateEmployee = (id, data) => API.put(`api/employees/${id}/`, data);
